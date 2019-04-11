5493f1c4e80e0b8900adf5fdaaa0a55a2d0f8a50
JE-lee <1245183029@qq.com>
2019-04-11 20:33:47 +0800

    dynamic import 测试

--- demo1/index.js
+++ demo1/index.js
@@ -2,7 +2,8 @@ import './common.scss';
 import './inner/common.css';
 import './external/external.css';
 import { sayHello } from '../lib/hello'
-
+const print = import('./dynamic-import')
+console.log('print', print)
 console.log(sayHello())
 console.log('add new content')
 console.log('add new content 2')
